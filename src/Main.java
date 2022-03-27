b04f41a756c96bd8b6eb585ee6d0b3bfc99897bf
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2022-03-27 16:07:24 +0000

    Attempting to work with DB

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main  (String[] args) {
+    System.out.println("Hello");
+    // establishing connection to the database to test if it works
+    SqlConnection.Connect();
+  }
+}
