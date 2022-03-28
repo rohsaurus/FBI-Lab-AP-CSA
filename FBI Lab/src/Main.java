b302b791d05295304f7fab8380ca2fb901d5953b
rohsaurus <rohanparikh04@gmail.com>
2022-03-28 12:50:49 -0400

    Finished Case Two and Case Three

--- FBI Lab/src/Main.java
+++ FBI Lab/src/Main.java
@@ -27,6 +27,22 @@ public class Main {
 
 
     // case two: 
-    
+      // list of names of men and women who drive a Chevy, Ford, or Honda and have a salary above fiften thousand dollars a year and savings below two thousand dollars
+    ArrayList<String> caseTwo = SqlConnection.caseTwo();
+    System.out.println("\n\n\nPrinting all suspects who match the following parameters: Men and women who drive a Chevy, Ford, or Honda and have a Salary of above $15k, and savings below $2k.\n");
+    System.out.println("Name\tAddress\tState\tAge\tSex\tSalary\tSavings\tCar\tYear");
+    // enhanced for loop to print the data
+    for (String toPrint : caseTwo) {
+      System.out.println(toPrint);
+    }
+    // case three
+    // list of suspects that are female - Make and year of the automobile they are driving
+    ArrayList<String> caseThree = SqlConnection.caseThree();
+    System.out.println("\n\n\nPrinting all the suspects who match the following parameters: Females and the make and year of the automobile they drive.");
+    System.out.println("Sex\tMake\tYear");
+    // enhanced for loop to print the data
+    for (String toPrint : caseThree) {
+      System.out.println(toPrint);
+    }
   }
 }
--- FBI Lab/src/SqlConnection.java
+++ FBI Lab/src/SqlConnection.java
@@ -1,3 +1,4 @@
+
 /**
  * Rohan Parikh
  * FBI Lab
@@ -25,57 +26,58 @@ public class SqlConnection {
             e.printStackTrace();
             System.out.println("Connection failed");
         }
-       /* finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                System.out.println("Connection failed");
-            }
-
-        */
+        /*
+         * finally {
+         * try {
+         * if (conn != null) {
+         * conn.close();
+         * }
+         * 
+         * } catch (SQLException ex) {
+         * ex.printStackTrace();
+         * System.out.println("Connection failed");
+         * }
+         * 
+         */
 
         return conn;
     }
 
-
-    public static void insert(String name, String address, String state, int age, String sex, int Salary, int savings, String car, int year) {
-      String sql = "INSERT INTO Main(Name, Address, State, Age, Sex, Salary, Savings, Car, Year) VALUES(?,?,?,?,?,?,?,?,?,?)";
-        try{
+    public static void insert(String name, String address, String state, int age, String sex, int Salary, int savings,
+            String car, int year) {
+        String sql = "INSERT INTO Main(Name, Address, State, Age, Sex, Salary, Savings, Car, Year) VALUES(?,?,?,?,?,?,?,?,?,?)";
+        try {
             Connection conn = Connect();
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, name);
-            pstmt.setString(2,address);
-            pstmt.setString(3,state);
-            pstmt.setInt(4,age);
-            pstmt.setString(5,sex);
-            pstmt.setInt(6,Salary);
-            pstmt.setInt(7,savings);
-            pstmt.setString(8,car);
-            pstmt.setInt(9,year);
-          // executing SQL insertion statement
+            pstmt.setString(2, address);
+            pstmt.setString(3, state);
+            pstmt.setInt(4, age);
+            pstmt.setString(5, sex);
+            pstmt.setInt(6, Salary);
+            pstmt.setInt(7, savings);
+            pstmt.setString(8, car);
+            pstmt.setInt(9, year);
+            // executing SQL insertion statement
             pstmt.executeUpdate();
-           // conn.commit();
+            // conn.commit();
             conn.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    public static void selectAll(){
+
+    public static void selectAll() {
         String sql = "SELECT * FROM Main ORDER BY Name";
 
         try {
             Connection conn = Connect();
-            Statement stmt1  = conn.createStatement();
-            ResultSet rs    = stmt1.executeQuery(sql);
+            Statement stmt1 = conn.createStatement();
+            ResultSet rs = stmt1.executeQuery(sql);
             System.out.println(rs);
             // loop through the result set
-
 
             System.out.println("Name\tAddress\tState\tAge\tSex\tSalary\tSavings\tCar\tYear");
             while (rs.next()) {
@@ -90,7 +92,8 @@ public class SqlConnection {
                 String _car = rs.getString("Car");
                 int _year = rs.getInt("Year");
                 // printing output
-                System.out.println(_name + "\t" + _address + "\t" + _state + "\t" + _age + "\t" + _sex + "\t" + _Salary + "\t" + _savings + "\t" + _car + "\t" + _year);
+                System.out.println(_name + "\t" + _address + "\t" + _state + "\t" + _age + "\t" + _sex + "\t" + _Salary
+                        + "\t" + _savings + "\t" + _car + "\t" + _year);
             }
             // closing connection
             conn.close();
@@ -98,7 +101,8 @@ public class SqlConnection {
             e.printStackTrace();
         }
     }
-    public static ArrayList<String> caseOne () {
+
+    public static ArrayList<String> caseOne() {
         String sql = "SELECT * FROM Main WHERE Sex='M' AND Car='Ford' AND Salary > 20000 AND Age>30;";
         // arraylist to store and return
         ArrayList<String> results = new ArrayList<String>();
@@ -110,20 +114,76 @@ public class SqlConnection {
             ResultSet rs = stm1.executeQuery(sql);
             // looping through the result set, adding the values to the arraylist
             while (rs.next()) {
-                results.add(rs.getString("Name") + "\t" + rs.getString("Address") +"\t" + rs.getString("State") + "\t" + rs.getInt("Age") + "\t" + rs.getString("Sex") + "\t" + rs.getInt("Salary") + "\t" + rs.getInt("Savings") + "\t" + rs.getString("Car") + "\t" + rs.getInt("Year"));
+                results.add(rs.getString("Name") + "\t" + rs.getString("Address") + "\t" + rs.getString("State") + "\t"
+                        + rs.getInt("Age") + "\t" + rs.getString("Sex") + "\t" + rs.getInt("Salary") + "\t"
+                        + rs.getInt("Savings") + "\t" + rs.getString("Car") + "\t" + rs.getInt("Year"));
             }
             // closing connection
             conn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("There was a SQL error.");
             e.printStackTrace();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Some other generic error... Printing stack trace");
             e.printStackTrace();
         }
         // returning the arraylist with the speicifed values to be sorted in main
         return results;
+    }
+
+    public static ArrayList<String> caseTwo () {
+        String sql = "SELECT * FROM Main WHERE (Car='Chevy' OR Car='Ford' OR Car='Honda') AND Salary>15000 AND Savings<2000;";
+        // arraylist to store and return
+        ArrayList<String> results = new ArrayList<String>();
+        // try and catch loop to attempt to connect to the DB and grab the values needed
+        try {
+            Connection conn = Connect();
+            // creating statement and executing SQL statement
+            Statement stm1 = conn.createStatement();
+            ResultSet rs = stm1.executeQuery(sql);
+            // looping through the result set, adding the values to the arraylist
+            while (rs.next()) {
+                results.add(rs.getString("Name") + "\t" + rs.getString("Address") +"\t" + rs.getString("State") + "\t" + rs.getInt("Age") + "\t" + rs.getString("Sex") + "\t" + rs.getInt("Salary") + "\t" + rs.getInt("Savings") + "\t" + rs.getString("Car") + "\t" + rs.getInt("Year"));}
+            // closing the connection
+            conn.close();
+    }
+    catch (SQLException e) {
+        System.out.println("There was a SQL error.");
+        e.printStackTrace();
+    }
+    catch (Exception e) {
+        System.out.println("Some other generic error... Printing stack trace");
+        e.printStackTrace();
+    }
+    return results;
+    }
+
+    public static ArrayList<String> caseThree() {
+        String sql = "SELECT Sex,Car,Year FROM Main WHERE Sex='F';";
+        // arraylist to store and return the values
+        ArrayList<String> results = new ArrayList<String>();
+        try {
+            Connection conn = Connect();
+            // creating statement and executing SQL statement
+            Statement stm1 = conn.createStatement();
+            ResultSet rs = stm1.executeQuery(sql);
+            // looping through the result set, adding the values to the arraylist
+            while (rs.next()) {
+                results.add(rs.getString("Sex") +"\t" + rs.getString("Car") + "\t" + rs.getInt("Year"));
+        }
+        // closing the db connection
+        conn.close();
+    }
+    // catch statements to catch exception
+    catch (SQLException e) {
+        System.out.println("There was a SQL error.");
+        e.printStackTrace();
+    }
+    catch (Exception e) {
+        System.out.println("Some other generic error... Printing stack trace");
+        e.printStackTrace();
+    }
+    // returning the arraylist
+    return results;
     }
 }
